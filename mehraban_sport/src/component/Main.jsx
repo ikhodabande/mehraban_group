166da2e1ce6d7d77e18a8c6fe48fb3219e3f986c
Amirmohammad <amii_mhmd@yahoo.com>
2024-05-17 01:03:34 +0330

    adding some styles

--- mehraban_sport/src/component/Main.jsx
+++ mehraban_sport/src/component/Main.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <section name="Main" className='w-full h-screen  z-20 flex flex-col items-center justify-end'>
+    <section name="Main" className='w-full h-screen  flex flex-col items-center justify-end'>
+
+      <div className='w-[95%] h-[100px] rounded-xl bg-black'></div>
          {/* offers */}
-         <div className='w-full h-[60%] bg-[#ffffff3d] grid grid-cols-4 gap-10 px-10'>
-           <div>ss</div>
-           <div>ss</div>
-           <div>ss</div>
-           <div>ss</div>
+         <div className='w-full h-[40%] bg-[#ffffff3d] font-iranyekanBold grid grid-cols-4 gap-4 px-10 my-10'>
+           <div className='bg-black relative rounded-xl'>
+            <p className='text-2xl absolute bottom-8 right-8 text-end text-[#adadad]'>بهترین‌های<br />کراپ</p>
+           </div>
+           <div className='bg-black relative rounded-md'>
+            <p className='text-2xl absolute bottom-8 right-8 text-end text-[#adadad]'>بهترین‌های<br />کراپ</p>
+           </div>
+           <div className='bg-black relative rounded-md'>
+            <p className='text-2xl absolute bottom-8 right-8 text-end text-[#adadad]'>بهترین‌های<br />کراپ</p>
+           </div>
+           <div className='bg-black relative rounded-md'>
+            <p className='text-2xl absolute bottom-8 right-8 text-end text-[#adadad]'>بهترین‌های<br />کراپ</p>
+           </div>
          </div>
          
     </section>
